e9a3927311b9180636d69099abb957028511595e
Wilker Amorim Gomes <wilker.amorim@strategymanager.com.br>
2014-04-28 18:30:27 -0300

    Atualização

---
+++ phpmailer.php
@@ -0,0 +1,25 @@
+﻿<?php	
+	ini_set('display_errors', '1');	
+	
+	error_reporting(E_ALL);
+	//error_reporting(E_STRICT);
+
+	require("mailchimp/class.mailchimp.php");
+		
+	$fname	= $_POST['FNAME'];
+	$lname	= $_POST['LNAME'];		
+	$email	= $_POST['EMAIL'];		
+	$fone	= $_POST['PHONE'];		
+	
+	$MailChimp = new \drewm\MailChimp('8400dfbc1c91bc97d6b37a6a806150df-us8');
+	$result = $MailChimp->call('lists/subscribe', array(
+					'id'                => '13f1fa9e8c',
+					'email'             => array('email'=> $email),
+					'merge_vars'        => array('FNAME'=>$fname, 'LNAME'=>$lname, 'PHONE'=>$fone),
+					'double_optin'      => false,
+					'update_existing'   => true,
+					'replace_interests' => false,
+					'send_welcome'      => false,
+				));
+	echo json_encode($result);
+?>
